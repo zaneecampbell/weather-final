dc315d6e424d329f1b6fb7f950cdf0be5bdfe479
Zane Campbell <zanegitinit@gmail.com>
2019-01-18 21:08:47 -0800

    foasjdlfjkasdf;lsiud

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,8 @@ module.exports = (env) => {
   return {
     entry: ['babel-polyfill', './src/app.js'],
     output: {
-      path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js',
-      publicPath: "/portfolio-final/",
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js'
     },
     module: {
       rules: [{
